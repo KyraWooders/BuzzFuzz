8caef5ada45d08808057cdf85533957a23c1d6c7
s198061 <s198061@LAF-7E868C>
2019-08-30 09:09:46 -0500

    Add project files.

---
+++ BuzzFuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuzzFuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] FizzBuzzArray = new int[101];
+            for (int i = 0; i < FizzBuzzArray.Length; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                Console.WriteLine(i);
+                
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
